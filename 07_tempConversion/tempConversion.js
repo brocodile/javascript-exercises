40d994c084bba6073057a364f170c8032e885698
brocodile <dakzsh24@gmail.com>
2024-08-12 16:20:45 +0530

    Do Exercise7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(faren) {
+  let celci = (faren - 32)*(5/9);
+  celci = Math.round(celci * 10) / 10;
+  return celci;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celci) {
+  let faren = celci * (9/5) + 32;
+  faren = Math.round(faren * 10) / 10;
+  return faren;
 };
 
 // Do not edit below this line
