631e4802ecee99d3e4062f674374b0e82d648ccb
brocodile <dakzsh24@gmail.com>
2024-08-12 14:46:33 +0530

    Do Second Exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(word, n) {
+    if (n < 0){
+        return 'ERROR';
+    }
+    let newWord = '';
+    while(n > 0){
+        newWord += word;
+        n--;
+    }
+    return newWord;
 };
 
 // Do not edit below this line
