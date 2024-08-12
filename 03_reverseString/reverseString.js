959a36d0eb700f5d8737f6a9475cbdcfd1e136db
brocodile <dakzsh24@gmail.com>
2024-08-12 14:53:38 +0530

    Do Exercise3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let newWord = '';
+    for (i = word.length - 1; i > -1; i--){
+        newWord += word[i];
+    }
+    return newWord;
 };
 
 // Do not edit below this line
