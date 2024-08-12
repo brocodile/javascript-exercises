2d557a0213f14f1b5b3d12a88245b640abe9c210
brocodile <dakzsh24@gmail.com>
2024-08-12 15:40:38 +0530

    Do Exercise4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(nums, ...args) {
+    let i = 0;
+    for(let i = 0; i<args.length;i++){
+        for(let j = 0; j < nums.length; j++){
+            if(nums[j] === args[i]){
+                nums.splice(j, 1);
+                j -= 1;
+            }
+        }
+    }
+    return nums;
 };
 
 // Do not edit below this line
