447a65bcc681a9ed2ed250a2de356ae2f956abb0
brocodile <dakzsh24@gmail.com>
2024-09-03 19:54:43 +0530

    Complete Exercise11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,6 @@
-const getTheTitles = function() {
-
-};
-
+const getTheTitles = function(books) {
+    const titles = books.map(item => item.titles);
+    return titles;
+}
 // Do not edit below this line
 module.exports = getTheTitles;
