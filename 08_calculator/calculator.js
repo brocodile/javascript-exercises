eecc869793b60d521e29d327965840bca8fc240c
brocodile <dakzsh24@gmail.com>
2024-09-03 14:48:42 +0530

    Do Exercise8 calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a+b;	
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((accm, val) => accm+val, 0);	
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((accm, val) => accm*val, 1);
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+  return num ** pow;	
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if(num == 0) return 1;
+  // return num * factorial(num-1);	 // recursive solution
+  // Iterative solution
+  let res = 1;
+  while(num > 0){
+    res *= num;
+    num--;
+  }
+  return res;
 };
 
 // Do not edit below this line
