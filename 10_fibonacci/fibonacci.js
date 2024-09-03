a985c9e1a831f25ee57ab9b4395015a04e662556
brocodile <dakzsh24@gmail.com>
2024-09-03 19:45:25 +0530

    Complete Exercise10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(val) {
+    if(typeof val == "string"){
+        val = parseInt(val);
+    }
+    if(val < 0){
+        return "OOPS";
+    }
+    if(val == 0){
+        return 0;
+    }
 
+    let first = 1;
+    let second = 1;
+    for(let i=2; i < val;i++){
+        [first, second] = [second, first+second];
+    }
+    return second;
 };
 
 // Do not edit below this line
