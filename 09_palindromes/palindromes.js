82488f774318752aec834fc8f4b6437492eda25a
brocodile <dakzsh24@gmail.com>
2024-09-03 15:20:58 +0530

    Do Exercise9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    // Solution 1 :
+    const avoid = ' !,.';
+    let i = 0;
+    let j = word.length - 1;
+    while(i<j){
+        while(i<j && avoid.includes(word[i])) i++;
+        while(i<j && avoid.includes(word[j])) j--;
+        if(word[i].toLowerCase() != word[j].toLowerCase()) return false;
+        i++;
+        j--;
+    }
+    return true;
 };
 
 // Do not edit below this line
