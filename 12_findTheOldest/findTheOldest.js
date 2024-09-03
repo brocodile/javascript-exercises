cdb451e2612dc6b0dcef9fc5941263ac23c21708
brocodile <dakzsh24@gmail.com>
2024-09-03 20:44:35 +0530

    Complete Exercise12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldest = people.reduce((accm, val) => {
+        let yOD;
+        let aYOD;
+        if(["yearOfDeath"] in val) {yOD = val.yearOfDeath;}
+        else {yOD = ((new Date()).getFullYear());}
+        if(["yearOfDeath"] in accm) {aYOD = accm.yearOfDeath;}
+        else {aYOD = ((new Date()).getFullYear());}
 
+        let lived = yOD - val.yearOfBirth;
+        let accmage = aYOD - accm.yearOfBirth;
+        if(lived > accmage){
+            accm = val;
+        }
+        return accm;
+    },people[0]);
+    return oldest;
 };
 
 // Do not edit below this line
